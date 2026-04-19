286956388585a3a2802b7daaafc07ffb95954ac6
Kevin de Borst <39211508+kdeborst@users.noreply.github.com>
2026-04-19 11:37:49 +0200

    Add styling to BottomNavigation component

--- src/components/navigation/BottomNavigation/BottomNavigation.jsx
+++ src/components/navigation/BottomNavigation/BottomNavigation.jsx
@@ -4,7 +4,7 @@ function BottomNavigation() {
     return (
         <nav className='bottom-navigation'>
             <div className='page-container bottom-navigation-container'>
-                <a href='#' className='bottom-navigation-link-active'>Home</a>
+                <a href='#' className='bottom-navigation-link active'>Home</a>
                 <a href='#' className='bottom-navigation-link'>Opties</a>
                 <a href='#' className='bottom-navigation-link'>Prijzen</a>
                 <a href='#' className='bottom-navigation-link bottom-navigation-link-right'>Ondernemer</a>
