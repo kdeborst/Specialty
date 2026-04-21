6b0b377e1079b8031db880833d36d643f86783dc
Kevin de Borst <39211508+kdeborst@users.noreply.github.com>
2026-04-21 14:45:09 +0200

    Add LoginForm component styling

--- src/features/auth/components/LoginForm/LoginForm.jsx
+++ src/features/auth/components/LoginForm/LoginForm.jsx
@@ -11,33 +11,33 @@ function LoginForm({
             <div className='login-form-inner'>
                 <h1 className='login-form-title'>{title}</h1>
                 <p className='login-form-subtitle'>{subtitle}</p>
+                <form className='login-form-fields'>
+                    <label className='login-form-input-wrapper'>
+                        <Mail size={18} />
+                        <input 
+                            type='email'
+                            name='email'
+                            placeholder='Mailadres'
+                        />
+                    </label>
+                    <p className='login-form-helper-text'>
+                        U dient een e-mailadres te gebruiken...
+                    </p>
+                    <label className='login-form-input-wrapper'>
+                        <Lock size={18} />
+                        <input
+                            type='password'
+                            name='password'
+                            placeholder='wachtwoord'
+                        />
+                    </label>
+                    <p className='login-form-helper-text'>
+                        Onbekende email en wachtwoord combinatie...
+                    </p>
+                    <button type='submit' className='login-form-primary-button'>Login</button>
+                    <button type='button' className='login-form-link-button'>wachtwoord vergeten?</button>
+                </form>
             </div>
-            <form className='login-form-fields'>
-                <label className='login-form-input-wrapper'>
-                    <Mail size={18} />
-                    <input 
-                        type='email'
-                        name='email'
-                        placeholder='Mailadres'
-                    />
-                </label>
-                <p className='login-form-helper-text'>
-                    U dient een e-mailadres te gebruiken...
-                </p>
-                <label className='login-form-input-wrapper'>
-                    <Lock size={18} />
-                    <input
-                        type='password'
-                        name='password'
-                        placeholder='wachtwoord'
-                    />
-                </label>
-                <p className='login-form-hepler-text'>
-                    Onbekende email en wachtwoord combinatie...
-                </p>
-                <button type='submit' className='login-form-primary-button'>Login</button>
-                <button type='button' className='login-form-link-button'>wachtwoord vergeten?</button>
-            </form>
         </div>
     )
 }
